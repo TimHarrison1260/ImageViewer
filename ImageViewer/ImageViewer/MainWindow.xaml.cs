b1a72ff3c77956e2154ae182ab6bbad6fec68345
Tim Harrison <TimHarrison1260@gmail.com>
2013-01-31 20:56:57 +0000

    First Animation of selected Image
    Done with code behind and PropertyChanging event.  XAML causes problem
    and not correct for the moment

--- ImageViewer/ImageViewer/MainWindow.xaml.cs
+++ ImageViewer/ImageViewer/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using System.Windows.Media.Animation;       // for Storyboard class
+
 using ImageViewer.Infrastructure;
 using ImageViewer.Model;
 using System.IO;
@@ -25,12 +27,83 @@ namespace ImageViewer
     /// </summary>
     public partial class MainWindow : Window
     {
+        //  Hold a reference to the instance of the viewModel used for ths page.
+        private SlideShowViewModel _vm;
+
+
         /// <summary>
         /// Constructor for the MainWindow.
         /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+
+            //  Get hold of the instance of the ViewModel for this page.  It's
+            //  defined as a Window.Resource, so we can look for that resource.
+            _vm = (SlideShowViewModel) this.FindResource("ImagesSource");
+            //  Attach a handler for the PropertyChanging event of the ViewModel.
+            _vm.PropertyChanging += _vm_PropertyChanging;
         }
+
+
+        private void currentImage_Unloaded(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("currentImage_Unloaded event has fired");
+        }
+
+        private void currentImage_Loaded(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("currentImage_Loaded event has fired");
+        }
+
+        private void currentImage_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+            //MessageBox.Show("currentImage_SourceUpdated event has fired");
+        }
+
+        private void currentImage_TargetUpdated(object sender, DataTransferEventArgs e)
+        {
+            //MessageBox.Show("currentImage_TargetUpdated event has fired");
+        }
+
+        private void imageList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //MessageBox.Show("imageList_SelectionChanged event has fired");
+            //  Fade image out
+
+            //  Change image source
+            //var slide = (Slide) this.imageList.SelectedItem;
+            //var imageSource = slide.Image;
+            //this.currentImage.Source = imageSource;
+            //  Fade new image in
+            Storyboard fadeIn = (Storyboard)FindResource("FadeInImage");
+            Image theImage = this.currentImage;
+            if (this.currentImage != null)
+                fadeIn.Begin(this.currentImage);
+        }
+
+        private void imageList_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+            //MessageBox.Show("imageList_SourceUpdated event has fired");
+        }
+
+
+
+        /// <summary>
+        /// Event handler for the PropertyChanging event 
+        /// </summary>
+        /// <param name="sender">An instance of the ovject that raosed the event.</param>
+        /// <param name="e">An instance of the EventArgs for the PropertyChanging event.</param>
+        private void _vm_PropertyChanging(object sender, System.ComponentModel.PropertyChangingEventArgs e)
+        {
+            MessageBox.Show(string.Format("Property '{0}' is about to change", e.PropertyName));
+            //  Potentially we can start an animation on the image that's there before the new images is loaded
+            //  Fade old image out
+            Storyboard fadeOut = (Storyboard)FindResource("FadeOutImage");
+            
+            if (this.currentImage != null)
+                fadeOut.Begin(this.currentImage);
+        }
+
     }
 }
--- ImageViewer/ImageViewer/Model/SlideShowViewModel.cs
+++ ImageViewer/ImageViewer/Model/SlideShowViewModel.cs
@@ -5,10 +5,11 @@ using ImageViewer.Infrastructure;
 
 using System.IO;
 using System.Windows.Media.Imaging;
+using System.ComponentModel;
 
 namespace ImageViewer.Model
 {
-    public class SlideShowViewModel
+    public class SlideShowViewModel : INotifyPropertyChanged
     {
         private ImageRepository repository = new ImageRepository();
 
@@ -45,6 +46,23 @@ namespace ImageViewer.Model
             return _slides;
         }
 
+        private Slide _currentImage;
+        /// <summary>
+        /// Gets or set the current slide
+        /// </summary>
+        public Slide CurrentSlide
+        {
+            get { return _currentImage; }
+            set
+            {
+                if (value != null)
+                {
+                    OnPropertyChanging("CurrentSlide");
+                    _currentImage = value;
+                    OnPropertyChanged("CurrentSlide");
+                }
+            }
+        }
 
         /// <summary>
         /// Load the data into the model.
@@ -55,19 +73,35 @@ namespace ImageViewer.Model
             var imagePaths = repository.GetImagePaths();
             foreach (var p in imagePaths)
             {
-                Uri uri = new Uri(p);
-                BitmapImage img = new BitmapImage(uri);
-                Slide s = new Slide();
-                s.Image = img;
-                s.Name = Path.GetFileName(p);
-                //{
-                //    Image = new BitmapImage(new Uri(p)),
-                //    Name = Path.GetFileName(p)
-                //};
+                Slide s = new Slide()
+                {
+                    Image = new BitmapImage(new Uri(p)),
+                    Name = Path.GetFileName(p)
+                };
                 _slides.Add(s);
-               
+            }
+            //  Set the current image to the first one in the collection
+            _currentImage = _slides[0];
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        protected void OnPropertyChanged(string PropertyName)
+        {
+            var changedEvent = PropertyChanged;
+            if (changedEvent != null)
+            {
+                changedEvent(this, new PropertyChangedEventArgs(PropertyName));
             }
         }
 
+        protected void OnPropertyChanging(string PropertyName)
+        {
+            var changingEvent = PropertyChanging;
+            if (changingEvent != null)
+                changingEvent(this, new PropertyChangingEventArgs(PropertyName));
+        }
     }
 }
